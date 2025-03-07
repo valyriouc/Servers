684cb502621fb65a84323e14ed90a8ef5a2b6b53
valcor <valyrious.informatics@gmail.com>
2025-03-07 14:32:49 +0100

    implement a logging framework

---
+++ Servers.Logging/ConsoleLogger.cs
@@ -0,0 +1,34 @@
+﻿namespace Servers.Logging;
+
+public class ConsoleLogger : ILogger<ConsoleLogger>
+{
+    private readonly DateTime _start;
+    
+    public ConsoleLogger(DateTime start) => _start = start;
+
+    public static ConsoleLogger Start(DateTime start) => new(start);
+
+    public void Log(string source, LogState state, string message)
+    { 
+        DateTime current = DateTime.UtcNow;
+        Console.Write($"{current} {source}");
+        Console.ForegroundColor = state.ToConsoleColor();
+        Console.Write($" {state} ");
+        Console.ResetColor();
+        Console.WriteLine($"{message}");
+    }
+
+    public void Log(string source, Exception ex) => 
+        Log(source, LogState.Error, ex.ToString());
+}
+
+internal static class LoggerStateExtensions
+{
+    public static ConsoleColor ToConsoleColor(this LogState self) => self switch
+    {
+        LogState.Error => ConsoleColor.Red,
+        LogState.Warning => ConsoleColor.Yellow,
+        LogState.Info => ConsoleColor.Green,
+        _ => throw new NotSupportedException()
+    };
+}
---
+++ Servers.Logging/ILogger.cs
@@ -0,0 +1,17 @@
+﻿namespace Servers.Logging;
+
+public enum LogState
+{
+    Info,
+    Warning,
+    Error
+}
+
+public interface ILogger<T> where T : ILogger<T>
+{
+    public static abstract T Start(DateTime start);
+    
+    public void Log(string source, LogState state, string message);
+
+    public void Log(string source, Exception ex);
+}
